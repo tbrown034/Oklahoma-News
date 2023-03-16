5afd1b64fb783853cfd7cc696a44d8a14efade20
tbrown034 <tbrown034@gmail.com>
2023-03-16 01:20:35 -0500

    added filter and search features

--- src/Components/Headline.js
+++ src/Components/Headline.js
@@ -6,6 +6,7 @@ const apiKey = '773b4f5d3b9d45beb2af765631140be6';
 const Headline = () => {
   const [headlines, setHeadlines] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [sortType, setSortType] = useState('titleAsc');
   
   useEffect(() => {
     axios.get('https://newsapi.org/v2/everything', {
@@ -27,16 +28,37 @@ const Headline = () => {
   const filteredHeadlines = headlines.filter(headline => {
     const searchRegex = new RegExp(searchTerm, 'i');
     return searchRegex.test(headline.title) || searchRegex.test(headline.description) || searchRegex.test(headline.source.name);
+  }).sort((a, b) => {
+    if (sortType === 'titleAsc') {
+      return a.title.localeCompare(b.title);
+    } else if (sortType === 'titleDesc') {
+      return b.title.localeCompare(a.title);
+    } else if (sortType === 'dateAsc') {
+      return new Date(a.publishedAt) - new Date(b.publishedAt);
+    } else {
+      return new Date(b.publishedAt) - new Date(a.publishedAt);
+    }
   });
 
   return (
     <div className="myHeadline">
-        <div className="headlineTop">
-      <h2>Latest Oklahoma Headlines</h2>
-      <label>
-        Search:
-        <input type="text" value={searchTerm} onChange={event => setSearchTerm(event.target.value)} />
-      </label>
+      <div className="headlineTop">
+        <h2>Latest Oklahoma Headlines</h2>
+        <div>
+          <label>
+            Search:
+            <input type="text" value={searchTerm} onChange={event => setSearchTerm(event.target.value)} />
+          </label>
+          <label>
+            Sort by:
+            <select value={sortType} onChange={event => setSortType(event.target.value)}>
+              <option value="titleAsc">Title (A-Z)</option>
+              <option value="titleDesc">Title (Z-A)</option>
+              <option value="dateAsc">Date (oldest first)</option>
+              <option value="dateDesc">Date (latest first)</option>
+            </select>
+          </label>
+        </div>
       </div>
       <table>
         <thead>
